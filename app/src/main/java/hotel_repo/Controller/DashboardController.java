cd07c798c496b4814a78c72673fdb4cd8f3a290f
DianaBoaca <diana.boaca@student.upt.ro>
2022-05-09 19:40:30 +0300

    HRA-7 - added Log out button in dashboard and created Edit Page

--- app/src/main/java/hotel_repo/Controller/DashboardController.java
+++ app/src/main/java/hotel_repo/Controller/DashboardController.java
@@ -14,6 +14,29 @@ import hotel_repo.Model.User;
 
 public class DashboardController {
 
+    @FXML
+    public void handleEditAction() {
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getClassLoader().getResource("editPage.fxml"));
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    @FXML
+    public void handleLogoutAction(ActionEvent event) {
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getClassLoader().getResource("register.fxml"));
+            Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
